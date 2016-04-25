df25d12a4dc52eca409f8ba9d09d67ccbf521f7f
Daniel Ilchyshyn <kruberlick@gmail.com>
2016-04-25 21:23:55 +0300

    Initial MapView setup with initial location in Honolulu

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
+//import the MapKit framework
+import MapKit
+
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    //MARK: Properties
+    
+    let regionRadius: CLLocationDistance = 1000 //distance value (in meters) for the correct zoom
+    
+    
+    
+    //MARK: Map view setup
+    
+    //center map view on over the given location with zoom value, initialized before
+    func centerMapOnLocation(location: CLLocation) {
+        
+        //setup square region
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        
+        //set region to map view
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    
+    // MARK: Overrided methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        
+        //center map's location to initial location
+        centerMapOnLocation(initialLocation)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
