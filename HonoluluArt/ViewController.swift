97cbf9dbb70376b3499d4d93bb248b8c9798d485
Daniel Ilchyshyn <kruberlick@gmail.com>
2016-04-25 21:06:12 +0300

    Initial Commit

---
+++ HonoluluArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HonoluluArt
+//
+//  Created by Данил Ильчишин on 4/25/16.
+//  Copyright © 2016 KRUBERLICK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
